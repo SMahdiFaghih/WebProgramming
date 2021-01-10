ae1e6b8c095ac3816848b2c417eb3352b73bf159
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-01-10 19:29:26 +0330

    added SignUp and Get Posts

--- HW3/nodejs/index.js
+++ HW3/nodejs/index.js
@@ -75,6 +75,50 @@ ParseApp.get("/signin", (req, response) => {
   return response.status(405).json({ "message": "Only `Post` Method is Valid"});
 });
 
+ParseApp.post("/signup", (request, response) => {
+  console.log("POST /signup");
+  const email = request.body.email;
+  const password = request.body.password;
+  if (!validateEmail(email))
+  {
+    return response.status(400).json({ "message": "filed `email` is not valid"});
+  }
+  if (Object.keys(request.body).length > 2)
+  {
+    return response.status(400).json({"message": "Request Length should be 2"});
+  }
+  if (password.length < 5)
+  {
+    return response.status(400).json({"message": "filed `password`.length should be gt 5"});
+  }
+  const query = new Parse.Query(Users);
+  query.equalTo("email", email);
+  query.limit(1);
+  query.find().then((results) => {
+    if (results.length == 0)
+    {
+      const User = Parse.Object.extend("Users");
+      const newUser = new User();
+      newUser.save({
+        email: email,
+        password: password
+      })
+      .then(() => {
+        return response.status(201).json({"message": "user has been created."});
+      }, () => {
+        console.log('signUp failed');
+      });
+    }
+    return response.status(409).json({"message": "email already exist."});
+  }, () => {
+    console.log('find failed');
+  });
+});
+
+ParseApp.get("/signup", (req, response) => {
+  return response.status(405).json({"message": "Only `Post` Method is Valid"});
+});
+
 function validateEmail(email)
 {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -93,9 +137,17 @@ function makeToken(length)
   return result;
 }
 
+ParseApp.get("/post", (req, response) => {
+  const query = new Parse.Query(Posts);
+  query.find().then((posts) => {
+    return response.status(200).json({ "posts": posts});
+  }, (error) => {
+    console.log('find posts failed');
+  });
+}
 
 
-
+/*
 //Insert
 const GameScore = Parse.Object.extend("Test");
 const gameScore = new GameScore();
@@ -149,3 +201,4 @@ query3.get("BJEQeUXkk9")
 }, (error) => {
   console.log(error);
 });
+*/
