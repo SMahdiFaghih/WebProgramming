e9734fe0c6f980ea576486d89a587fc266ce1cd4
fatteme <fatt3me@gmail.com>
2021-02-11 01:18:30 +0330

    HW3: change request routes

--- HW3/nodejs2/app.js
+++ HW3/nodejs2/app.js
@@ -19,15 +19,15 @@ const limiter = rateLimit({
     windowMs: 60 * 60 * 1000,
     message: 'Too Many Request from this IP, please try again in an hour'
 });
-app.use('/api', limiter);
+app.use('/', limiter);
 app.use(express.json({
     limit: '15kb'
 }));
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
-app.use('/api', publicRoutes);
-app.use('/api/admin', userRoutes);
+app.use('/', publicRoutes);
+app.use('/admin', userRoutes);
 app.use('*', (req, res, next) => {
     const err = new AppError(404, 'fail', 'undefined route');
     next(err, req, res, next);
