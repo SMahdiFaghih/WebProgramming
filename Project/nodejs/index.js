14edaa8b88e4895d208c8ce6f133d66e1df865a9
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-02-10 19:25:19 +0330

    added SignUp functions

--- Project/nodejs/index.js
+++ Project/nodejs/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 const port = 8080;
 
+LecturerEmails = ["aabaam@gmail.com", "mrbahrami@gmail.com"]
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -16,25 +18,73 @@ var con = mysql.createConnection({
     database: "web_project"
 });
 
-con.connect(function(err) 
+createDatabase();
+signUpLecturer("aabaam@gmail.com", 12345, "mahdi");
+
+function signUpStudent(email, password, username)
 {
-    if (err)
-    {
-        console.log(err);
-    } 
-    console.log("Connected!");
-    con.query("CREATE DATABASE IF NOT EXISTS web_project;", function(err)
+    con.query('SELECT * FROM student WHERE email = ? LIMIT 1', [email], function (err, result, fields) 
     {
         if (err)
         {
             console.log(err);
+            return;
+        } 
+        if (result.length != 0)
+        {
+            console.log("student exists with this email");
+            return;
         }
-        else
+        con.query('INSERT INTO student SET email = ?, password = ?, username = ?', [email, password, username], function (err, result) 
         {
-            console.log("Database created");
-        } 
+            if (err)
+            {
+                console.log(err);
+            } 
+            else
+            {
+                console.log("1 record inserted");
+            }
+        });
     });
-    con.query("CREATE TABLE IF NOT EXISTS student (email VARCHAR(128) PRIMARY KEY NOT NULL, password VARCHAR(128) NOT NULL, username VARCHAR(128) NOT NULL)", function(err)
+}
+
+function signUpLecturer(email, password, username)
+{
+    if (!LecturerEmails.includes(email))
+    {
+        console.log("lecturer email not existed at all!");
+        return;
+    }
+    con.query('SELECT * FROM lecturer WHERE email = ? LIMIT 1', [email], function (err, result, fields) 
+    {
+        if (err)
+        {
+            console.log(err);
+            return;
+        } 
+        if (result.length != 0)
+        {
+            console.log("lecturer exists with this email");
+            return;
+        }
+        con.query('INSERT INTO lecturer SET email = ?, password = ?, username = ?', [email, password, username], function (err, result) 
+        {
+            if (err)
+            {
+                console.log(err);
+            } 
+            else
+            {
+                console.log("1 record inserted");
+            }
+        });
+    });
+}
+
+function createDatabase()
+{
+    con.connect(function(err) 
     {
         if (err)
         {
@@ -42,65 +92,87 @@ con.connect(function(err)
         } 
         else
         {
-            console.log("Table student created");
-        } 
+            console.log("Connected!");
+            con.query("CREATE DATABASE IF NOT EXISTS web_project;", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                    console.log("Database created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS student (email VARCHAR(128) PRIMARY KEY NOT NULL, password VARCHAR(128) NOT NULL, username VARCHAR(128) NOT NULL)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table student created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS lecturer (email VARCHAR(128) PRIMARY KEY NOT NULL, password VARCHAR(128) NOT NULL, username VARCHAR(128) NOT NULL)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table lecturer created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS form (form_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, lecturer_email VARCHAR(128) NOT NULL, title VARCHAR(128) NOT NULL, description VARCHAR(2048) NOT NULL, status ENUM ('Open' , 'Closed') NOT NULL, FOREIGN KEY (lecturer_email) references lecturer (email) on delete cascade on update cascade)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table form created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS form_fields (form_id INT NOT NULL, description VARCHAR(128) NOT NULL, required TINYINT(1) NOT NULL, type ENUM ('TextField' , 'CheckList') NOT NULL, checklist_options VARCHAR(256), PRIMARY KEY (form_id, description), FOREIGN KEY (form_id) references form (form_id) on delete cascade on update cascade)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table form_fields created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS filled_forms (form_id INT NOT NULL, student_email VARCHAR(128) NOT NULL, result ENUM ('Accepted' , 'Rejected', 'Pending') NOT NULL, PRIMARY KEY (form_id, student_email), FOREIGN KEY (form_id) references form (form_id) on delete cascade on update cascade, FOREIGN KEY (student_email) references student (email) on delete cascade on update cascade)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table filled_forms created");
+                } 
+            });
+            con.query("CREATE TABLE IF NOT EXISTS filled_forms_data (form_id INT NOT NULL, student_email VARCHAR(128) NOT NULL, field_description VARCHAR(128) NOT NULL, data VARCHAR(512), PRIMARY KEY (form_id, student_email, field_description), FOREIGN KEY (student_email) references student (email) on delete cascade on update cascade, FOREIGN KEY (form_id, field_description) references form_fields (form_id, description) on delete cascade on update cascade)", function(err)
+            {
+                if (err)
+                {
+                    console.log(err);
+                } 
+                else
+                {
+                    console.log("Table filled_forms_data created");
+                } 
+            });
+        }
     });
-    con.query("CREATE TABLE IF NOT EXISTS lecturer (email VARCHAR(128) PRIMARY KEY NOT NULL, password VARCHAR(128) NOT NULL, username VARCHAR(128) NOT NULL)", function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        } 
-        else
-        {
-            console.log("Table lecturer created");
-        } 
-    });
-    con.query("CREATE TABLE IF NOT EXISTS form (form_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, lecturer_email VARCHAR(128) NOT NULL, title VARCHAR(128) NOT NULL, description VARCHAR(2048) NOT NULL, status ENUM ('Open' , 'Closed') NOT NULL, FOREIGN KEY (lecturer_email) references lecturer (email) on delete cascade on update cascade)", function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        } 
-        else
-        {
-            console.log("Table form created");
-        } 
-    });
-    con.query("CREATE TABLE IF NOT EXISTS form_fields (form_id INT NOT NULL, description VARCHAR(128) NOT NULL, required TINYINT(1) NOT NULL, type ENUM ('TextField' , 'CheckList') NOT NULL, checklist_options VARCHAR(256), PRIMARY KEY (form_id, description), FOREIGN KEY (form_id) references form (form_id) on delete cascade on update cascade)", function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        } 
-        else
-        {
-            console.log("Table form_fields created");
-        } 
-    });
-    con.query("CREATE TABLE IF NOT EXISTS filled_forms (form_id INT NOT NULL, student_email VARCHAR(128) NOT NULL, result ENUM ('Accepted' , 'Rejected', 'Pending') NOT NULL, PRIMARY KEY (form_id, student_email), FOREIGN KEY (form_id) references form (form_id) on delete cascade on update cascade, FOREIGN KEY (student_email) references student (email) on delete cascade on update cascade)", function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        } 
-        else
-        {
-            console.log("Table filled_forms created");
-        } 
-    });
-    con.query("CREATE TABLE IF NOT EXISTS filled_forms_data (form_id INT NOT NULL, student_email VARCHAR(128) NOT NULL, field_description VARCHAR(128) NOT NULL, data VARCHAR(512), PRIMARY KEY (form_id, student_email, field_description), FOREIGN KEY (student_email) references student (email) on delete cascade on update cascade, FOREIGN KEY (form_id, field_description) references form_fields (form_id, description) on delete cascade on update cascade)", function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        } 
-        else
-        {
-            console.log("Table filled_forms_data created");
-        } 
-    });
-});
+}
 
 
 /*app.post("/", (request, response) => {
