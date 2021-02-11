dbad11d8ac32bb37157bfa2c86b4af191c4935fe
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-02-11 18:02:37 +0330

    added some more of required functions

--- Project/nodejs/index.js
+++ Project/nodejs/index.js
@@ -17,14 +17,15 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "<yourPassword>",
-    database: "web_project"
+    database: "web_project",
+    multipleStatements: true,
 });
 
 var successfulRequestResponse = "OK";
 var failedRequestResponse = "Error";
 
 createDatabase();
-closeForm("aabaam@gmail.com", 12);
+responseToForm("aabaam@gmail.com", "s@gmail.com", 27, "Accepted");
 
 app.post("/signup", (request, response) => {
     console.log("POST /signup");
@@ -49,6 +50,90 @@ app.post("/signup", (request, response) => {
         return response.status(400).end('role is invalid');
     }
 });
+
+function responseToForm(lecturer_email, student_email, form_id, result)
+{
+    con.query('UPDATE filled_forms JOIN form ON form.form_id = filled_forms.form_id SET result = ? WHERE filled_forms.form_id = ? AND student_email = ? AND lecturer_email = ?', [result, form_id, student_email, lecturer_email], function (err, result, fields) 
+    {
+        if (err)
+        {
+            console.log(err);
+            return failedRequestResponse;
+        } 
+        console.log("1 form answered");
+        return successfulRequestResponse;
+    });
+}
+
+function deleteForm(form_id, student_email)
+{
+    con.query('DELETE FROM filled_forms WHERE form_id = ? AND student_email = ? AND result = "Pending"', [form_id, student_email], function (err, result) 
+    {
+        if (err)
+        {
+            console.log(err);
+            return failedRequestResponse;
+        } 
+        if (result.affectedRows != 1)
+        {
+            console.log("your form is not Pending or not existed");
+            return failedRequestResponse;
+        }
+        con.query('DELETE FROM filled_forms_data WHERE form_id = ? AND student_email = ?', [form_id, student_email], function (err, result) 
+        {
+            if (err)
+            {
+                console.log(err);
+                return failedRequestResponse;
+            } 
+            console.log(result.affectedRows + " records deleted from filled_forms_data table");
+            return successfulRequestResponse;
+        });            
+    }); 
+}
+
+function editFilledForm(form_id, student_email, fields)
+{
+    var updatedFields = fields.map(item => [item.data, form_id, student_email, item.field_name]);
+    var queries = '';
+    updatedFields.forEach(function (item) {
+        queries += mysql.format("UPDATE filled_forms_data SET data = ? WHERE form_id = ? AND student_Email = ? AND field_name = ?; ", item);
+    });
+
+    con.query(queries, function (err, result) 
+    {
+        if (err)
+        {
+            console.log(err);
+            return failedRequestResponse;
+        } 
+        console.log(fields.length + " records edited in filled_forms_data table");
+        return successfulRequestResponse;
+    });      
+}
+
+function fillForm(form_id, student_email, fields)
+{
+    con.query('INSERT IGNORE INTO filled_forms SET form_id = ?, student_email = ?, result = "Pending"', [form_id, student_email], function (err, result) 
+    {
+        if (err)
+        {
+            console.log(err);
+            return failedRequestResponse;
+        } 
+        console.log("1 record inserted to filled_forms table");
+        con.query('INSERT INTO filled_forms_data (form_id, student_email, field_name, data) VALUES ?', [fields.map(item => [form_id, student_email, item.field_name, item.data])], function (err, result) 
+        {
+            if (err)
+            {
+                console.log(err);
+                return failedRequestResponse;
+            } 
+            console.log(fields.length + " records inserted to filled_forms_data table");
+            return successfulRequestResponse;
+        });            
+    });   
+}
 
 function closeForm(lecturer_email, form_id)
 {
